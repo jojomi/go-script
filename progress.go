e5ef89a110b8bca9510c4218b5594234357be2d2
jojomi <dev.jojomi@yahoo.com>
2017-10-14 15:16:41 +0200

    Add ProgressReader

--- progress.go
+++ progress.go
@@ -1,8 +1,12 @@
 package script
 
 import (
+	"io"
+	"os"
+
 	"github.com/gernest/wow"
 	"github.com/gernest/wow/spin"
+	"gopkg.in/cheggaaa/pb.v1"
 )
 
 // ActivityIndicatorCustom returns an activity indicator as specified.
@@ -16,4 +20,22 @@ func (c Context) ActivityIndicatorCustom(text string, typ spin.Name) *wow.Wow {
 // ActivityIndicator returns an activity indicator with specified text and default animation.
 func (c Context) ActivityIndicator(text string) *wow.Wow {
 	return c.ActivityIndicatorCustom(text, spin.Dots)
+}
+
+// ProgressReader returns a reader that is able to visualize read progress.
+func (c Context) ProgressReader(reader io.Reader, size int) (io.Reader, *pb.ProgressBar) {
+	bar := pb.New(size).SetUnits(pb.U_BYTES)
+
+	// create proxy reader
+	return bar.NewProxyReader(reader), bar
+}
+
+// ProgressFileReader returns a reader that is able to visualize read progress for a file.
+func (c Context) ProgressFileReader(f *os.File) (io.Reader, *pb.ProgressBar, error) {
+	size, err := f.Stat()
+	if err != nil {
+		return nil, nil, err
+	}
+	r, bar := c.ProgressReader(f, int(size.Size()))
+	return r, bar, nil
 }
